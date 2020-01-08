173391334d9a18033638db8183bc28a2bf05635f
SUZIELLEN <suelenrodriguesmachado18@gmail.com>
2020-01-08 11:31:23 -0300

    teste

--- app/src/main/java/com/example/adiligencia/login.java
+++ app/src/main/java/com/example/adiligencia/login.java
@@ -2,7 +2,9 @@ package com.example.adiligencia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void telaFooter (View view){
+
+        Intent intent = new Intent (this, footer.class);
+        startActivity(intent);
     }
 }
